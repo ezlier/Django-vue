768d7bc6a99068b058430e70c27f8e3f268129bc
ezlier <86654833@qq.com>
2025-10-11 22:33:07 +0800

    1

---
+++ frontend/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import api from '@/utils/request'
+
+const message = ref('Loading...')
+
+onMounted(async () => {
+  const res = await api.get('/hello/')
+  message.value = res.data.message
+})
+</script>
+
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
